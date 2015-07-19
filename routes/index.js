f3b5a113664ebf2b2453edef9aa4ef011b35bfc1
eligiuz <ecachon.tc@uttab.edu.mx>
2015-07-19 00:19:10 -0500

    Correcciones, agregado de la página author
    Se agrego la página author con los créditos del autor así como
    correcciones menores en la dirección del trabajo

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Autor' });
+});
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+
 
 module.exports = router;
